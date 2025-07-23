cf5d52ad3bf12a3ce667912f2627ee396bc6d395
Luiz Rocha <luiz.rocha@pumatronix.com.br>
2025-07-23 09:44:32 -0300

    Update dmp_train_script.py

--- dmp_train_script.py
+++ dmp_train_script.py
@@ -83,9 +83,8 @@ class Trainer:
         batch = {k: v.to(self.local_rank) for k, v in batch.items()}
         if self.rank == 0:
             self.schedule.step(input_ids=batch['input_ids'], attention_mask=batch['attention_mask'])
-        #elif self.rank == self.world_size - 1:
-        #    outputs = self.schedule.step(input_ids=batch['input_ids'], attention_mask=batch['attention_mask'], 
-        #                                 labels=batch['labels'])
+        elif self.rank == self.world_size - 1:
+            outputs = self.schedule.step(labels=batch['labels'])
         else:
             outputs = self.schedule.step()
         torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=self.max_grad_norm)
